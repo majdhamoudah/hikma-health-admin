a6dbcec7b668f6c8bf99d54c7ad9418f6018723c
majdhamoudah <42590417+majdhamoudah@users.noreply.github.com>
2024-05-12 14:28:40 -0600

    fix next

--- next.config.js
+++ next.config.js
@@ -1,3 +1,11 @@
-module.exports ={
-  output:"standalone",
-}
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+  enabled: process.env.ANALYZE === 'true',
+});
+
+module.exports = withBundleAnalyzer({
+  reactStrictMode: false,
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  output: 'standalone',
+});
